c4eef395c055a6ed317dec40bb6d871b16641e6c
Kumar-laxmi <laxmikantk2002@gmail.com>
2022-03-25 05:37:10 +0530

    Minor changes in Form Validation

--- JS/Validations/ValidateLogin.js
+++ JS/Validations/ValidateLogin.js
@@ -1,26 +1,26 @@
 import {ValidateEmail} from './Email_Validate';
 import {CheckPassword} from './Password_Validate';
 
-function ValidateLoginPage()
+function ValidateLoginPage(LogIn_Form)
 {
-    var mail = document.LogIn_Form.Email_Input;
-    var password = document.LogIn_Form.Password_Input;
+    var mail = LogIn_Form.Email_Input.value;
+    var password = LogIn_Form.Password_Input.value;
 
-    if(ValidateEmail(mail) && CheckPassword())
+    if(ValidateEmail(mail) && CheckPassword(password))
     {
         return true;
     }
-    else if(!ValidateEmail(mail) && CheckPassword())
+    else if(!ValidateEmail(mail) && CheckPassword(password))
     {
         alert('Invalid Email Format detected!!!')
         return false;
     }
-    else if(ValidateEmail(mail) && !CheckPassword())
+    else if(ValidateEmail(mail) && !CheckPassword(password))
     {
         alert('Invalid Password Format detected!!!')
         return false;
     }
-    else if(!ValidateEmail(mail) && !CheckPassword())
+    else if(!ValidateEmail(mail) && !CheckPassword(password))
     {
         alert('Invalid Email Format & Password Format detected!!!')
         return false;
--- JS/Validations/ValidateRegistration.js
+++ JS/Validations/ValidateRegistration.js
@@ -3,28 +3,28 @@ import {CheckPassword} from './Password_Validate';
 import {ValidateName} from './Name_Validate';
 import {ConfirmPassword} from './Confirm_Password_Validate';
 
-function ValidateRegistration() 
+function ValidateRegistration(Registration_Form) 
 {
-    var name = document.Registration_Form.Name_Input;
-    var mail = document.Registration_Form.Email_Input;
-    var password = document.Registration_Form.Password_Input;
-    var confirm_password = document.Registration_Form.Confirm_Password_Input;
+    var name = Registration_Form.Name_Input.value;
+    var mail = Registration_Form.Email_Input.value;
+    var password = Registration_Form.Password_Input.value;
+    var confirm_password = Registration_Form.Confirm_Password_Input.value;
 
     if(!ValidateName(name)) {
         alert('Invalid Name Format Detected');
         return false;
     }
-    else if(!ValidateEmail(mail) && CheckPassword())
+    else if(!ValidateEmail(mail) && CheckPassword(password))
     {
         alert('Invalid Email Format detected!!!')
         return false;
     }
-    else if(ValidateEmail(mail) && !CheckPassword())
+    else if(ValidateEmail(mail) && !CheckPassword(password))
     {
         alert('Invalid Password Format detected!!!')
         return false;
     }
-    else if(!ValidateEmail(mail) && !CheckPassword())
+    else if(!ValidateEmail(mail) && !CheckPassword(password))
     {
         alert('Invalid Email Format & Password Format detected!!!')
         return false;
