a1727c1d610072ee7612f992df1885c86f14abeb
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-05-24 19:03:57 +0530

    Added blogsdata folder for sample data Added blog component as given in screenshot Added CSS and handled responsiveness using CSS

---
+++ tscomm/Blogsdata/Blogs.js
@@ -0,0 +1,83 @@
+import sample from "../public/sample.jpeg";
+export const Blogsdata = [
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+  {
+    id: Math.random() * 10000,
+    image: sample,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eligendi?",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, corporis ratione, corrupti consectetur nostrum ad sint quaerat labore repudiandae, ullam eligendi a officiis. Tempora eveniet delectus ullam minima sunt repellendus cupiditate illum minus eum! Sapiente laboriosam quo id iusto vitae nostrum labore nobis exercitationem suscipit perferendis sed, ut enim tenetur soluta tempora sequi asperiores deleniti! Nemo dicta provident dolor quo ratione atque nam saepe. Veniam modi ratione facilis debitis repellendus blanditiis quaerat, error excepturi? Quae quaerat quia iure excepturi odit sunt eos illo, consequuntur voluptatum debitis deserunt id? Maiores, quam. Harum, temporibus doloremque asperiores obcaecati reprehenderit cum voluptatum ipsam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis consectetur recusandae tenetur! Hic odio totam quibusdam officia provident consectetur? Ipsa, sed ea! Reiciendis voluptates harum consequuntur rerum inventore nihil at, assumenda dolorum quam soluta amet nesciunt id reprehenderit atque molestiae aut quisquam dicta maxime eveniet. Eligendi voluptatum numquam, natus fugiat a velit perspiciatis dolorum, possimus veniam expedita quos. Eius voluptas officia fuga magni sit. Laudantium, nesciunt. Velit nemo, architecto, deserunt dolore porro impedit natus nostrum voluptatem, ipsa totam itaque consectetur!",
+    readtime: "6 min",
+    date: "12 May",
+  },
+];
