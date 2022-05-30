bdfe34cbd948ff0e8131dbdb74ee2015911d69d9
piyusha2001 <ppiyusha2001@gmail.com>
2022-05-30 16:23:16 +0530

    page using reference created

---
+++ tscomm/Data/Cards.js
@@ -0,0 +1,32 @@
+export const CardsData = [
+	{
+		image: 'https://thumbs.dreamstime.com/b/yellow-textured-plain-background-wallpaper-design-use-text-image-yellow-textured-plain-background-wallpaper-137515345.jpg',
+		subheading: 'Tokens and Data Credits',
+		about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ',
+	},
+	{
+		image: 'https://thumbs.dreamstime.com/b/yellow-textured-plain-background-wallpaper-design-use-text-image-yellow-textured-plain-background-wallpaper-137515345.jpg',
+		subheading: 'Tokens and Data Credits',
+		about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ',
+	},
+	{
+		image: 'https://thumbs.dreamstime.com/b/yellow-textured-plain-background-wallpaper-design-use-text-image-yellow-textured-plain-background-wallpaper-137515345.jpg',
+		subheading: 'Tokens and Data Credits',
+		about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ',
+	},
+	{
+		image: 'https://thumbs.dreamstime.com/b/yellow-textured-plain-background-wallpaper-design-use-text-image-yellow-textured-plain-background-wallpaper-137515345.jpg',
+		subheading: 'Tokens and Data Credits',
+		about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ',
+	},
+	{
+		image: 'https://thumbs.dreamstime.com/b/yellow-textured-plain-background-wallpaper-design-use-text-image-yellow-textured-plain-background-wallpaper-137515345.jpg',
+		subheading: 'Tokens and Data Credits',
+		about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ',
+	},
+	{
+		image: 'https://thumbs.dreamstime.com/b/yellow-textured-plain-background-wallpaper-design-use-text-image-yellow-textured-plain-background-wallpaper-137515345.jpg',
+		subheading: 'Tokens and Data Credits',
+		about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ',
+	},
+];
